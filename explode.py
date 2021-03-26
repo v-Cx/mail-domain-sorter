6265dd86476526cacec06a85e76f59a285107202
Ibrahim <ibrhimaydiin@gmail.com>
2021-03-26 13:40:13 +0300

    version 0.2 | Change File name, types, and fixed errors.

--- explode.py
+++ explode.py
@@ -1,3 +1,12 @@
+"""
+Sellthing.co
+Styx
+Andwiseb
+
+version: 0.2
+"""
+
+
 import tkinter as tk
 from tkinter import filedialog
 import os
@@ -5,6 +14,8 @@ from os import system
 import os.path
 import time
 system("title " + "Mail Domain Sorter - Sellthing - Styx")
+from datetime import date
+from datetime import datetime
 
 root = tk.Tk()
 root.withdraw()
@@ -24,10 +35,12 @@ www.sellthing.co | Styx
 print(logTextSellhting)
 
 def getStarted():
+    
     print('Please separate each word with a comma (,). Example: test, test1, test2')
     getWords = input("Enter the words you want to split :")
     replace = getWords.replace(', ', ',').replace(',', ',').replace(' ', '').replace('\s+ ', '').replace(' ,', '').replace(' \s+', '')
     splitWords = ""
+
     if(getWords == ""):
         print('Please enter words.')
         getStarted();
@@ -72,6 +85,10 @@ def splitWord(word):
         If the user is scanning more than one word, we check it.
         This is due to the following for and if codes.
     """
+
+    now = datetime.now()
+    getTime = now.strftime("%H-%M-%S")
+
     if(isinstance(word, list)):
         print("Completing the process please wait.")
         system("title " + "Mail Domain Sorter - Process has started.")
@@ -89,20 +106,20 @@ def splitWord(word):
                         If there is a word in the file that the user searched for, a file of that word is opened below. Example: test1.txt
                     """
                     i += 1
-                    open('result/' + os.path.basename(str(words) + '.txt'), 'w+')
+                    open('result/' + os.path.basename(str(words) + " - " + getTime + '.txt'), 'w+')
                     findList.append(line)
             if control == True:
                 """
                     The reason for checking the "Control" variable is to prevent creating a new file from each line in the file.
                 """
-                open("result/" + str(words) + '.txt', "w").writelines(findList)
+                open("result/" + str(words) + " - " + getTime + '.txt', "w").writelines(findList)
 
                 """ After all searches for a word are completed, we reset the records found. """
                 findList = []
             print("Generate Process is Starting.")
             time.sleep(0.5)
             print("---------------------------------------------------")
-            print(str(len(open('result/' + words +  '.txt').read().splitlines())) + " satır " + str(words) + " bulundu")
+            print(str(len(open('result/' + words + " - " + getTime +  '.txt').read().splitlines())) + " " + str(words) + " records found.")
             print("---------------------------------------------------")
             time.sleep(0.5)
                 
@@ -116,16 +133,16 @@ def splitWord(word):
             if word in line:
                 control = True
                 i += 1
-                open('result/' + os.path.basename(str(word) + '.txt'), 'w+')
+                open('result/' + os.path.basename(str(word) + " - " + getTime + '.txt'), 'w+')
                 findList.append(line)
         if control == True:
-            open("result/" + str(word) + '.txt', "w").writelines(findList)
+            open("result/" + str(word) + " - " + getTime + '.txt', "w").writelines(findList)
         else:
             print("No line found about " + word)
 
 
         print("---------------------------------------------------")
-        print(str(len(open('result/' + word +  '.txt').read().splitlines())) + " satır " + word + " bulundu")
+        print(str(len(open('result/' + word + " - " + getTime +  '.txt').read().splitlines())) + " " + word + " records found.")
         print("---------------------------------------------------")
         
     time.sleep(0.5)
